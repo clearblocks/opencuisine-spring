7a6120435ae91b5e4a9a6b26e830c0a0df87788f
Theo van Oostrum <theo@xel.nl>
2023-10-18 22:32:46 +0200

    Project setup

---
+++ src/test/java/nl/clearblocks/opencuisine/OpenCuisineApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.clearblocks.opencuisine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenCuisineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
